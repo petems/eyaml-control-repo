82166442e27d4fdb57048fe050f90d4a788e96ca
Peter Souter <p.morsou@gmail.com>
2016-02-26 12:13:28 +0000

    initial commit - Setup eyaml setup

---
+++ Puppetfile
@@ -0,0 +1,13 @@
+forge "http://forge.puppetlabs.com"
+
+mod "puppetlabs/inifile", :latest
+mod "puppetlabs/stdlib", :latest
+
+mod 'hiera',
+  :git => 'https://github.com/hunner/puppet-hiera',
+  :tag => '1.3.1'
+
+# Change the version of hiera-eyaml you want to test with the server
+mod 'hiera-eyaml',
+  :git => 'https://github.com/petems/hiera-eyaml',
+  :ref => 'refactor_highline_import'
---
+++ site/profile/manifests/puppetmaster_eyaml.pp
@@ -0,0 +1,47 @@
+class profile::puppetmaster_eyaml {
+
+  $hiera_yaml = "${::settings::confdir}/hiera.yaml"
+
+  class { '::hiera':
+    hierarchy  => [
+      'common',
+      'eyaml_common',
+    ],
+    logger            => 'console',
+    eyaml             => true,
+    backends          => ['yaml', 'eyaml', 'redis'],
+    datadir           => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
+    eyaml_datadir     => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
+    datadir_manage    => false,
+    create_keys       => false,
+    eyaml_private_key => 'puppet:///modules/profiles/keys/private_key.pkcs7.pem',
+    eyaml_public_key  => 'puppet:///modules/profiles/keys/public_key.pkcs7.pem',
+    notify            => Service['pe-puppetserver'],
+  }
+
+  ini_setting { 'puppet.conf hiera_config main section' :
+    ensure  => present,
+    path    => "${::settings::confdir}/puppet.conf",
+    section => 'main',
+    setting => 'hiera_config',
+    value   => $hiera_yaml,
+    notify  => Service['pe-puppetserver'],
+  }
+
+  ini_setting { 'puppet.conf hiera_config master section' :
+    ensure  => absent,
+    path    => "${::settings::confdir}/puppet.conf",
+    section => 'master',
+    setting => 'hiera_config',
+    value   => $hiera_yaml,
+    notify  => Service['pe-puppetserver'],
+  }
+
+  #remove the default hiera.yaml from the code-staging directory
+  #after the next code manager deployment it should be removed
+  #from the live codedir
+  file { '/etc/puppetlabs/code-staging/hiera.yaml' :
+    ensure => absent,
+  }
+
+}
